c8c4110d53c2c653e068f2adca64fcee70053c70
Tetiana1192 <tanya_strigun@ukr.net>
2022-06-10 19:06:02 +0300

    ллдд

--- js/01-vars.js
+++ js/01-vars.js
@@ -1,9 +1,14 @@
-let elementWidth = '50px';
- elementWidth = Number.parseInt(elementWidth);
-console.log('elementWidth: ', elementWidth);
+// const cart = [54, 28, 105, 70, 92, 17];
+// let total = 0;
+// for (const value of cart) {
+//    total += value; 
+// }
+// console.log('Total: ', total);
 
-
-
-
-
-
+const calculateTotalPrice = function (items) {
+    let total = 0;
+    for (const item of items) {
+        total += item;
+    }
+    return total; 
+};
